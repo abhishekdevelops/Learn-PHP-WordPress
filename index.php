2adf7f9810a186b6a6c049ad3b45271a6ec7e696
abhishek-web-design <abhishekvadnala10@gmail.com>
2025-04-08 13:24:41 +0530

    Add php-practice-theme project

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// output message
+echo "<h1>Welcome to PHP Learning</h1>";
+$name="Digital Web Trends";
+$goal="Become a WordPress Pro in 2 months";
+//output
+echo"<p>Hello I am learning PHP with {$name}.</p>";
+echo"<p>My Goal:{$goal}.</p>";
+?>
+
+<?php
+$name="Abhishek";
+$city="Mumbai";
+$language="PHP";
+$goal="Become a WordPress Developer";
+
+echo"<hr><h2>My Name is {$name}.</h2>";
+echo"<p>I stay in {$city}.</p>";
+echo"<p>My favourite language is {$language}.</p>";
+echo"<p>My Goal: {$goal}.</p>";
+?>
