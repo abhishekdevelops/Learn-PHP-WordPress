f0fcf29acffc10a274bc2c80e1a06e67f7fa8139
abhishek-web-design <abhishekvadnala10@gmail.com>
2025-04-21 15:17:57 +0530

    Renamed practice-theme to 01-practice-theme (PHP basics)

---
+++ 01-practice-theme/index.php
@@ -0,0 +1,20 @@
+<?php
+// output message
+echo "<h1>Welcome to PHP Learning</h1>";
+$goal="Become a WordPress Developer";
+//output
+echo"<p>Hello I am learning PHP</p>";
+echo"<p>My Goal:{$goal}.</p>";
+?>
+
+<?php
+$name="Abhishek";
+$city="Mumbai";
+$language="PHP";
+$goal="Become a WordPress Developer";
+
+echo"<hr><h2>My Name is {$name}.</h2>";
+echo"<p>I stay in {$city}.</p>";
+echo"<p>My favourite language is {$language}.</p>";
+echo"<p>My Goal: {$goal}.</p>";
+?>
